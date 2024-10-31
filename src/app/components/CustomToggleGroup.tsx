d2ac11fb701ca24733ab073f16edb5a19cc6823a
Roysignaler <roysignaler@gmail.com>
2024-10-31 14:25:44 +0100

    Footer

--- src/app/components/CustomToggleGroup.tsx
+++ src/app/components/CustomToggleGroup.tsx
@@ -5,7 +5,7 @@ import * as ToggleGroup from "@radix-ui/react-toggle-group";
 
 interface CustomToggleGroupProps {
   value: "male" | "female";
-  onValueChange: (value: "male" | "female") => void; // Updated to accept only "male" or "female"
+  onValueChange: (value: "male" | "female") => void;
   isWarmTheme: boolean;
 }
 
@@ -14,35 +14,41 @@ const CustomToggleGroup = ({
   onValueChange,
   isWarmTheme,
 }: CustomToggleGroupProps) => {
-  const toggleGroupItemClasses = `
-    flex items-center justify-center px-4 py-2 font-semibold cursor-pointer rounded 
-    ${
-      isWarmTheme
-        ? "bg-[#FFD8C2] text-[#8B4000]"
-        : "bg-[#C2E3FF] text-[#1B4D89]"
-    }
-    hover:bg-opacity-80 focus:z-10 focus:shadow-[0_0_0_2px] focus:outline-none
-    data-[state=on]:bg-opacity-90
-  `;
+  const getButtonClasses = (gender: "male" | "female") => {
+    const isSelected = value === gender;
+    return `
+      flex items-center justify-center px-4 py-2 font-semibold cursor-pointer rounded 
+      ${
+        isSelected
+          ? isWarmTheme
+            ? "bg-[#F94807] text-white"
+            : "bg-color-bright-100 text-white"
+          : isWarmTheme
+          ? "bg-[#FFD8C2] text-[#8B4000]"
+          : "bg-[#C2E3FF] text-[#1B4D89]"
+      }
+      hover:bg-opacity-80 focus:z-10 focus:shadow-[0_0_0_2px] focus:outline-none
+    `;
+  };
 
   return (
     <ToggleGroup.Root
       type="single"
       value={value}
-      onValueChange={(newValue) => onValueChange(newValue as "male" | "female")} // Cast newValue to match type
+      onValueChange={(newValue) => onValueChange(newValue as "male" | "female")}
       aria-label="Gender selection"
       className="flex space-x-2 mt-4"
     >
       <ToggleGroup.Item
         value="male"
-        className={toggleGroupItemClasses}
+        className={getButtonClasses("male")}
         aria-label="Male"
       >
         Male
       </ToggleGroup.Item>
       <ToggleGroup.Item
         value="female"
-        className={toggleGroupItemClasses}
+        className={getButtonClasses("female")}
         aria-label="Female"
       >
         Female
---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,43 @@
+// components/Footer.tsx
+
+interface FooterProps {
+  isWarmTheme: boolean;
+  toggleTheme: () => void;
+}
+
+export default function Footer({ isWarmTheme, toggleTheme }: FooterProps) {
+  return (
+    <footer
+      className={`w-full p-4 ${
+        isWarmTheme ? "bg-[#F94807]" : "bg-color-bright-100"
+      } text-white`}
+    >
+      <div className="max-w-screen-xl mx-auto border-b-[10px] border-white"></div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 max-w-screen-xl mx-auto py-4 text-lg">
+        <div className="flex justify-start items-center">
+          <div>
+            <h2 className="text-lg font-semibold">WattKG</h2>
+            <p>© 2024</p>
+          </div>
+        </div>
+        <div className="flex justify-center items-center">
+          <div>
+            <h2 className="text-lg font-semibold">About</h2>
+            <p>Design and code by Stefan Frederiksen</p>
+          </div>
+        </div>
+        <div className="flex justify-end items-center">
+          <div>
+            <h2 className="text-lg font-semibold">Feedback</h2>
+            <p>
+              Tell me{" "}
+              <a href="https://forms.gle/aDMDVmzJjn95rDT97">
+                <u>what you want to see</u>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
